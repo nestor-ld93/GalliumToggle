0a7ca17a7251a9ea85e4b8cd10170a0a09c8a873
Nestor <nestor.ld93@gmail.com>
2020-12-08 15:17:48 -0500

    (v1.0.0) Lanzamiento inicial.

---
+++ GalliumToggle.bash
@@ -0,0 +1,91 @@
+#!/bin/bash
+#==================================================================================
+#   +==========================================================================+  #
+#   |                         GalliumToggle v1.0.0                             |  #
+#   +==========================================================================+  #
+#   | -Ultima actualizacion: 07/12/2020                                        |  #
+#   +--------------------------------------------------------------------------+  #
+#   | -Copyright (C) 2020 NestorBase11 (nestor-ld93)                           |  #
+#   +--------------------------------------------------------------------------+  #
+#==================================================================================
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#=========================================================================
+
+modo="auto" #"auto" o "manual"
+variable_toggle="GALLIUM_HUD_TOGGLE_SIGNAL=10" #SIGUSR1
+comando_PID="ps -eo pid"
+PID=($($comando_PID))
+N=${#PID[@]}
+
+i=122
+Nf=0
+while [ $i -lt ${N-1} ]; do
+    comando1="grep -i ${variable_toggle} /proc/${PID[i]}/environ"
+    salida_comando=$(${comando1})
+    
+    if [[ $salida_comando == */proc/${PID[i]}/environ* ]]; then
+        #comando2="ps -p ${PID[i]} -o comm="
+        proceso=($(ps -p ${PID[i]} -o comm=))
+        
+        if [[ $proceso != *wine* && $proceso != *services.exe* && \
+              $proceso != *plugplay.exe* && $proceso != *explorer.exe* ]]; then
+            proceso_f[$Nf]=${proceso[0]}
+            PID_f[$Nf]=${PID[i]}
+            
+            if [[ $modo == "auto" ]]; then
+                echo "($i) - HUD detectado en: $1 - ${PID[i]}"
+                kill -n 10 ${PID[$i]}
+            fi
+            ((Nf++))
+        fi
+    fi
+    ((i++))
+done
+#clear
+
+if [[ $modo == "auto" ]]; then
+    printf "=====================================================\n"
+    echo "-> HUD activado/desactivado en [$Nf] procesos:"
+    printf "    N   PID - Proceso\n"
+    i=0
+    while [ $i -lt $Nf ]; do
+        printf "   [$i] ${PID_f[$i]} - ${proceso_f[$i]}\n"
+        ((i++))
+    done
+    printf "=====================================================\n"
+fi
+
+if [[ $modo == "manual" ]]; then
+    printf "=====================================================\n"
+    echo "-> HUD detectado en [$Nf] procesos:"
+    printf "    N   PID - Proceso\n"
+    i=0
+    while [ $i -lt $Nf ]; do
+        printf "   [$i] ${PID_f[$i]} - ${proceso_f[$i]}\n"
+        ((i++))
+    done
+    printf "=====================================================\n"
+    
+    echo "-> Activar/Desactivar HUD en el proceso:"
+    read entrada
+    if [[ $entrada -ge 0 && $entrada -lt $Nf ]]; then
+        printf "[$entrada] ${PID_f[$entrada]} - ${proceso_f[$entrada]}\n"
+        kill -n 10 ${PID_f[$entrada]}
+    else
+        echo "¡Entrada errónea!"
+        exit
+    fi
+fi
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-GalliumToggle
+# GalliumToggle
+Script en Bash que actúa como interruptor para activar/desactivar Gallium Hud siempre y cuando se emplee la variable `GALLIUM_HUD_TOGGLE_SIGNAL=10` al momento de lanzar las aplicaciones con Gallium Hud.
+
+Las funciones que realiza **GalliumToggle** son las siguientes:
+
+- Activar/desactivar Gallium Hud en todas las aplicaciones en ejecución.
+- Activar/desactivar Gallium Hud en las aplicaciones deseadas.
+
+## DEPENDENCIAS
+- Bash
+
+## REQUISITOS MÍNIMOS
+- Drivers Open-Source (Mesa) para GPUs AMD & Nvidia.
+
+## ¿CÓMO DESCARGAR?
+- Desde el botón [Code] o ejecute en un terminal:
+`git clone https://github.com/nestor-ld93/GalliumToggle`
+
+## ¿CÓMO EJECUTAR? (Para todas las aplicaciones)
+1. Ingresar a la carpeta clonada: `cd GalliumToggle`
+1. Ingresar al archivo `GalliumToggle.bash` modificar el argumento de la variable `modo` a `"auto"`. (Por defecto, se encuentra en `"auto"`)
+1. Establecer permisos de ejecución: `chmod +x GalliumToggle.bash`
+1. Ejecutar (no ingresar como superusuario): `./GalliumToggle.bash`
+
+## ¿CÓMO EJECUTAR? (Para las aplicaciones deseadas)
+1. Ingresar a la carpeta clonada: `cd GalliumToggle`
+1. Ingresar al archivo `GalliumToggle.bash` modificar el argumento de la variable `modo` a `"manual"`
+1. Establecer permisos de ejecución: `chmod +x GalliumToggle.bash`
+1. Ejecutar (no ingresar como superusuario): `./GalliumToggle.bash`
+1. Seleccionar la aplicación en el terminal cuando se solicite.
+
+## ¿CÓMO EJECUTAR? (Aplicaciones lanzadas como superusuario)
+1. Ingresar a la carpeta clonada: `cd GalliumToggle`
+1. Ingresar al archivo `GalliumToggle.bash` y anteponer al argumento de la variable `comando1` la palabra `sudo `, el resultado es: `sudo grep -i ${variable_toggle} /proc/${PID[i]}/environ`
+1. Establecer permisos de ejecución: `chmod +x GalliumToggle.bash`
+1. Ejecutar (no ingresar como superusuario): `./GalliumToggle.bash`
+1. Ingresar la contraseña del sistema en el terminal cuando se solicite.
+
+## ENLACES DE INTERÉS
+- [NotebookGPU](https://notebookgpu.blogspot.com/2018/08/monitorizar-graficos-hibridos-en-linux.html)
